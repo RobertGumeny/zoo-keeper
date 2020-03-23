b5d27e193f3524eac9c1bab6db264a831a6a8962
Robert Gumeny <robert.gumeny528@gmail.com>
2020-03-23 16:07:05 -0600

    H-O classes completed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class Alpaca { // NOTE Alpaca
-  constructor (name = '', color = '', gender = '', height = '', weight = 0, spits = true){
+  constructor(name = '', color = '', gender = '', height = '', weight = 0, spits = true){
     this.name = name
     this.color = color
     this.gender = gender
@@ -10,7 +10,7 @@ class Alpaca { // NOTE Alpaca
 }
 
 class Beaver { // NOTE Beaver
-  constructor (name = '', color = 'brown', gender = '', height = '', weight = 0, teethSize = "large"){
+  constructor(name = '', color = 'brown', gender = '', height = '', weight = 0, teethSize = "large"){
     this.name = name
     this.color = color
     this.gender = gender
@@ -21,7 +21,7 @@ class Beaver { // NOTE Beaver
 }
 
 class Crocodile { // NOTE Crocodile
-  constructor (name = '', color = 'green', gender = '', height = '', weight = 0, teethSize = '', tailLength = 0){
+  constructor(name = '', color = 'green', gender = '', height = '', weight = 0, teethSize = '', tailLength = 0){
     this.name = name
     this.color = color
     this.gender = gender
@@ -33,7 +33,7 @@ class Crocodile { // NOTE Crocodile
 }
 
 class Deer { // NOTE Deer
-  constructor (name = '', color = 'brown', gender = '', height = '', weight = 0, antlers = false, antlerSize = "none"){
+  constructor(name = '', color = 'brown', gender = '', height = '', weight = 0, antlers = false, antlerSize = "none"){
     this.name = name
     this.color = color
     this.gender = gender
@@ -45,7 +45,7 @@ class Deer { // NOTE Deer
 }
 
 class Elk { // NOTE Elk
-  constructor (name = '', color = 'brown', gender = '', height = '', weight = 0, antlers = false, antlerSize = "none"){
+  constructor(name = '', color = 'brown', gender = '', height = '', weight = 0, antlers = false, antlerSize = "none"){
     this.name = name
     this.color = color
     this.gender = gender
@@ -57,7 +57,7 @@ class Elk { // NOTE Elk
 }
 
 class Fox { // NOTE Fox
-  constructor (name = '', color = 'red', gender = '', height = '', weight = 0, tailLength = "short"){
+  constructor(name = '', color = 'red', gender = '', height = '', weight = 0, tailLength = "short"){
     this.name = name
     this.color = color
     this.gender = gender
@@ -68,7 +68,7 @@ class Fox { // NOTE Fox
 }
 
 class Gorilla { // NOTE Gorilla
-  constructor (name = '', color = 'black', gender = '', height = '', weight = 0, mean = true){
+  constructor(name = '', color = 'black', gender = '', height = '', weight = 0, mean = true){
     this.name = name
     this.color = color
     this.gender = gender
@@ -77,3 +77,91 @@ class Gorilla { // NOTE Gorilla
     this.mean = mean
   }
 }
+
+class Hawk { // NOTE Hawk
+  constructor(name = '', color = '', gender = '', wingspan = '', weight = 0, speed = 0){
+    this.name = name
+    this.color = color
+    this.gender = gender
+    this.wingspan = wingspan
+    this.weight = weight
+    this.speed = speed
+  }
+}
+
+class Impala { // NOTE Impala
+  constructor(name = '', color = 'brown', gender = '', height = '', weight = 0, speed = 0){
+    this.name = name
+    this.color = color
+    this.gender = gender
+    this.height = height
+    this.weight = weight
+    this.speed = speed
+  }
+}
+
+class Jackal { // NOTE Jackal
+  constructor(name = '', color = 'brown', gender = '', height = '', weight = 0) {
+    this.name = name
+    this.color = color
+    this.gender = gender
+    this.height = height
+    this.weight = weight
+  }
+}
+
+class Kangaroo { // NOTE Kangaroo
+  constructor(name = '', color = '', gender = '', height = '', weight = 0, tailLength = 'long'){
+    this.name = name
+    this.color = color
+    this.gender = gender
+    this.height = height
+    this.weight = weight
+    this.tailLength = tailLength
+  }
+}
+
+class Lemur { // NOTE Lemur
+  constructor(name = '', color = '', gender = '', height = '', weight = 0, tailLength = 'long') {
+    this.name = name
+    this.color = color
+    this.gender = gender
+    this.height = height
+    this.weight = weight
+    this.tailLength = tailLength
+  }
+}
+
+class Moose { // NOTE Moose
+  constructor(name = '', color = 'brown', gender = '', height = '', weight = 0, antlers = false, antlerSize = "") {
+    this.name = name
+    this.color = color
+    this.gender = gender
+    this.height = height
+    this.weight = weight
+    this.antlers = antlers
+    this.antlerSize = antlerSize
+  }
+}
+
+class Narwhal { // NOTE Narwhal
+  constructor(name = '', color = 'gray', gender = '', length = '', weight = 0, hornLength = "") {
+    this.name = name
+    this.color = color
+    this.gender = gender
+    this.length = length
+    this.weight = weight
+    this.hornLength = hornLength
+  }
+}
+
+class Octopus { // NOTE Octopus
+  constructor(name = '', color = '', gender = '', length = '', weight = 0) {
+    this.name = name
+    this.color = color
+    this.gender = gender
+    this.length = length
+    this.weight = weight
+  }
+}
+
